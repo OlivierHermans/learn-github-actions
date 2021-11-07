ea048af6fa23d8f336437edec3dde75b4e77babd
Olivier Hermans <oli4hermans@gmail.com>
2021-11-07 14:20:51 +0100

    Add HelloController

---
+++ src/main/java/be/olivierhermans/learngithubactions/HelloController.java
@@ -0,0 +1,13 @@
+package be.olivierhermans.learngithubactions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
