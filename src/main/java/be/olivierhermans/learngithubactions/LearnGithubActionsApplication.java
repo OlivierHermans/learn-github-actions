8fc59b098b75f4856564f76a0d95514857985d09
Olivier Hermans <oli4hermans@gmail.com>
2021-11-07 14:05:54 +0100

    Add Spring Boot project code

---
+++ src/main/java/be/olivierhermans/learngithubactions/LearnGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package be.olivierhermans.learngithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/olivierhermans/learngithubactions/LearnGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.olivierhermans.learngithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
